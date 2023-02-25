9f8a79f4fbe52f57b2f418684e730097305bb1b6
Ogbeide Godstime Osemenkhian <osemengt@gmail.com>
2023-02-25 13:50:16 +0000

    added Journal

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,7 +5,7 @@ class HomeActivities:
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
-      'message': 'Cloud is fun and sweet!',
+      'message': 'Cloud is fun and sweet and beautiful by GT!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
