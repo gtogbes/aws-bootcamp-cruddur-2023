4034834594043aff2c4d073aac702ed887863551
Ogbeide Godstime Osemenkhian <osemengt@gmail.com>
2023-04-04 22:50:50 +0000

    week 4 update

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Ogbeide Godstime', 'Gtogbes' ,'MOCK'),
-  ('Ogbeide Sydney', 'Sydney' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown' , 'test@gmail.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko' , 'test@gmail.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'Gtogbes' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
